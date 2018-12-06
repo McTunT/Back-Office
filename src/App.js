d49bec6034fd86049a21f546721f33aeb75faca8
James Teerayuth <teerayuth.james@gmail.com>
2018-12-06 11:08:13 +0700

    Few For Change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-//import RouterProps from "./ComponentsTest/RoutersProps";
-//import ListItemTest from "./ComponentsTest/ListItemTest";
-//import TakeOrder from "./Components/Takeorder/TakeOrder";
-//import ListItemLink from "./Components/Drawers/ListItemLink";
-//import TakeOrder from "./Components/Retail/TakeOrder";
-import Header from "./Components/Header/Header";
-//import Routers from "./Router/Routers";
-//import Login from "./Components/Login/Login";
-//import Calculator from "../src/ComponentsTest/Calculator";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Dashboard />
       </div>
     );
   }
